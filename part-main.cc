451fb8a9f6b9bd49d8e9591b15694bf03d8123f2
mint <kr480515@ohio.edu>
2023-07-20 21:59:44 -0400

    main for compilation

---
+++ part-main.cc
@@ -0,0 +1,14 @@
+/**
+ *    @file: parts-main.cc
+ *  @author: Riley Reedy
+ *    @date: 7/20/23
+ *   @brief: Defining the Transaction struct
+ *  
+ */
+
+#include "part.h"
+
+int			main(int argc, char **argv)
+{
+	return (0);
+}
